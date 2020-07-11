088a6b08a42f350159fd45f8652d147b5b214fb5
indiepopart <jimena@gmail.com>
2020-07-11 05:40:11 -0300

    replace restTemplate by webclient

--- cart-service/src/main/java/com/okta/developer/cartservice/service/PricingService.java
+++ cart-service/src/main/java/com/okta/developer/cartservice/service/PricingService.java
@@ -3,26 +3,27 @@ package com.okta.developer.cartservice.service;
 import com.okta.developer.cartservice.model.Cart;
 import com.okta.developer.cartservice.model.LineItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PricingService {
 
+
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     public Cart price(Cart cart){
         try {
-            HttpEntity httpEntity = new HttpEntity(cart);
-            ResponseEntity<Cart> response = restTemplate
-                    .exchange("http://pricing/pricing/price", HttpMethod.POST, httpEntity,
-                            Cart.class);
 
-            Cart priced = response.getBody();
+            Mono<Cart> response = webClient
+                    .post()
+                    .bodyValue(cart)
+                    .retrieve().bodyToMono(Cart.class);
+
+            Cart priced = response.block();
 
             for (int i = 0; i < priced.getLineItems().size(); i++) {
                 LineItem pricedLineItem = priced.getLineItems().get(i);
